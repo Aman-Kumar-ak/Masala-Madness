d8667678db41bc2880bc4f88e4b02249a5d27587
Aman-Kumar-ak <akumar22679@gmail.com>
2025-07-23 21:03:04 +0530

    Fixes of Pending Orders

--- frontend/src/pages/admin/PendingOrders.jsx
+++ frontend/src/pages/admin/PendingOrders.jsx
@@ -982,7 +982,6 @@ export default function PendingOrders() {
                   <button
                     onClick={handleQrConfirmPayment}
                     className="w-full py-3 rounded-lg font-medium bg-green-600 hover:bg-green-700 text-white shadow-md transition-colors text-lg flex items-center justify-center gap-2 mt-2"
-                    disabled={!isPrinterConnected}
                   >
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
